7c59a7dbb1c8bec0291b0262fb39478e0cc38f79
ddo <joeddo89@gmail.com>
2022-10-25 23:14:30 +0700

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+        es6: true,
+        browser: true,
+        node: true,
+    },
+    extends: ['eslint:recommended'],
+    plugins: ['svelte3'],
+    globals: {
+        chrome: 'readonly',
+        browser: 'readonly',
+    },
+    overrides: [
+        {
+            files: ['**/*.svelte'],
+            processor: 'svelte3/svelte3',
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 2019,
+        sourceType: 'module',
+    },
+    rules: {
+        'no-console': 1,
+    },
+}
